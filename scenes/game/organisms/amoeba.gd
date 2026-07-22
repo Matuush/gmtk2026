2cd8ed983634f0a043b4ef16c481bde4689ca2f8
Matuush <matus1@pull.cz>
2026-07-22 20:56:28 +0200

    Move amoeba randomly

--- scenes/game/organisms/amoeba.gd
+++ scenes/game/organisms/amoeba.gd
@@ -1,4 +1,17 @@
 class_name amoeba extends RigidBody2D
 
+var VELOCITY_CHANGE_LIMIT : float = .5
+
 func _ready() -> void:
 	print("have been made")
+
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	linear_velocity += Vector2(
+		randf_range(-VELOCITY_CHANGE_LIMIT, VELOCITY_CHANGE_LIMIT),
+		randf_range(-VELOCITY_CHANGE_LIMIT, VELOCITY_CHANGE_LIMIT)
+	)
+	move_and_collide(linear_velocity)
+	
