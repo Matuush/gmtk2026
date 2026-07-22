e4a6b0fa6fae1199c18bfaf0c350c514693a0d0a
Topeni <matysekz@centrum.cz>
2026-07-22 20:47:37 +0200

    add timer for spawning amoebas

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -8,17 +8,18 @@ var window_size : Vector2 = DisplayServer.window_get_size()
 func _physics_process(delta: float) -> void:
 	my_time += delta
 	#print(my_time)
-	var new_amoeba = amoeba_scene.instantiate()
-	new_amoeba.position.x = randf_range(0, window_size.x)
-	new_amoeba.position.y = randf_range(0, window_size.y)
-	#new_amoeba.position.x = randf_range(0, 10)
-	#new_amoeba.position.y = randf_range(0, 10)
-	print(new_amoeba.position)
-	add_child(new_amoeba)
 	#
-	#enemies.push_back(new_amoeba)
 	
 	#if my_time > 2.0:
 		#remove_child($Game)
 	#position.x += 100 * delta
 	pass
+
+
+func _on_spawn_timer_timeout() -> void:
+	var new_amoeba = amoeba_scene.instantiate()
+	new_amoeba.position.x = randf_range(0, window_size.x)
+	new_amoeba.position.y = randf_range(0, window_size.y)
+	enemies.push_back(new_amoeba)
+	#print(new_amoeba.position)
+	add_child(new_amoeba)
