d5d3c7e3bca0406b0aca9e3cbb82979ed4c892e9
Matuush <matus1@pull.cz>
2026-07-22 21:22:32 +0200

    Add amoeba skeleton implementation

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var AMOEBA_LIMIT : int = 20
+
 var my_time : float = 0.0
 var enemies : Array = []
 var window_size : Vector2 = DisplayServer.window_get_size()
@@ -17,9 +19,9 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_spawn_timer_timeout() -> void:
-	var new_amoeba = amoeba_scene.instantiate()
-	new_amoeba.position.x = randf_range(0, window_size.x)
-	new_amoeba.position.y = randf_range(0, window_size.y)
-	enemies.push_back(new_amoeba)
-	#print(new_amoeba.position)
-	add_child(new_amoeba)
+	if enemies.size() < AMOEBA_LIMIT:
+		var new_amoeba = amoeba_scene.instantiate()
+		
+		enemies.push_back(new_amoeba)
+		#print(new_amoeba.position)
+		add_child(new_amoeba)
